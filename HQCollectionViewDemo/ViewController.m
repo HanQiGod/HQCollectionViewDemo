38db861c39c47aeb730e55fc9061ad84f72aea5c
HanQiGod <1539841933@qq.com>
2018-10-10 11:27:49 +0800

    Initial Commit

---
+++ HQCollectionViewDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HQCollectionViewDemo
+//
+//  Created by 君凯商联网 on 2018/10/10.
+//  Copyright © 2018年 君凯商联网. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ HQCollectionViewDemoTests/HQCollectionViewDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  HQCollectionViewDemoTests.m
+//  HQCollectionViewDemoTests
+//
+//  Created by 君凯商联网 on 2018/10/10.
+//  Copyright © 2018年 君凯商联网. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HQCollectionViewDemoTests : XCTestCase
+
+@end
+
+@implementation HQCollectionViewDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
