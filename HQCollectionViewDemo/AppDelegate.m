c9b2bef393710c37a821903c32d56c6095fa463a
HanQiGod <1539841933@qq.com>
2018-11-06 10:38:52 +0800

    优化

--- HQCollectionViewDemo/AppDelegate.m
+++ HQCollectionViewDemo/AppDelegate.m
@@ -4,6 +4,8 @@
 //
 //  Created by Mr_Han on 2018/10/10.
 //  Copyright © 2018年 Mr_Han. All rights reserved.
+//  CSDN <https://blog.csdn.net/u010960265>
+//  GitHub <https://github.com/HanQiGod>
 //
 
 #import "AppDelegate.h"
